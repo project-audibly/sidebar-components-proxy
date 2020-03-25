3c7184a7485b7d76a5c3c30f4a3aa2488c32cabd
Joanna Wang <shiju.an362@gmail.com>
2020-03-24 23:02:49 -0700

    refactor server proxy api requests + render all four modules + add basic styling

--- server/index.js
+++ server/index.js
@@ -1,3 +1,26 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const bodyParser = require('body-parser');
+
+// const cors = require('cors');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// const port = 9000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(cors());
+
+// app.use('/api/mainSong', createProxyMiddleware({ target:"localhost:3003" }));
+// app.use('/songData', createProxyMiddleware({ target:"localhost:3001" }));
+// app.use('/songs', createProxyMiddleware({ target:"localhost:3010" }));
+// app.use('/api/*', createProxyMiddleware({ target:"localhost:4000" }));
+
+// app.use(express.static('client'));
+
+// app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -8,7 +31,7 @@ const port = 9000;
 const proxy = httpProxy.createProxyServer({});
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.all('/api/mainSong', (req, res) => {
   proxy.web(req, res, {
@@ -22,8 +45,19 @@ app.all('/songData', (req, res) => {
   });
 });
 
-app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
+app.all('/songs', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:3010"
+  })
+});
 
+app.all('/api/*', (req, res) => {
+  proxy.web(req, res, {
+    target: "http://localhost:4000"
+  })
+});
+
+app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
 
 
 // const express = require('express');
