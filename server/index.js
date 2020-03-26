afce6af7afd070e8e4757c330ee79472776024f3
Joanna Wang <shiju.an362@gmail.com>
2020-03-25 21:55:55 -0700

    fix broken proxy -- all modules rendering

--- server/index.js
+++ server/index.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// const bodyParser = require('body-parser');
-
-// const cors = require('cors');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// const port = 9000;
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(cors());
-
-// app.use('/api/mainSong', createProxyMiddleware({ target:"localhost:3003" }));
-// app.use('/songData', createProxyMiddleware({ target:"localhost:3001" }));
-// app.use('/songs', createProxyMiddleware({ target:"localhost:3010" }));
-// app.use('/api/*', createProxyMiddleware({ target:"localhost:4000" }));
-
-// app.use(express.static('client'));
-
-// app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
-
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -47,40 +24,14 @@ app.all('/songData', (req, res) => {
 
 app.all('/songs', (req, res) => {
   proxy.web(req, res, {
-    target: "http://localhost:3010"
+    target: "http://localhost:3131"
   })
 });
 
 app.all('/api/*', (req, res) => {
   proxy.web(req, res, {
-    target: "http://localhost:4000"
+    target: "http://localhost:4001"
   })
 });
 
 app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
-
-
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// const cors = require('cors');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// const port = 9000;
-
-// const { routes } = require('./config.json');
-
-// app.use(cors());
-// app.use(express.static(path.join(__dirname, 'client')));
-
-// for (route of routes) {
-//   app.use(route.route,
-//     createProxyMiddleware({
-//       target: route.address,
-//       pathRewrite: (path, req) => {
-//         return path.split('/').slice(2).join('/'); 
-//       }
-//     })
-//   );
-// }
-
-// app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
